7571aef4e280987a34fb0a4ae4b6ca52c2ec4875
jonbwalker <jonathan.b.walker0@gmail.com>
2019-05-27 11:32:50 -0700

    add increment/decrement methods

--- app/components/TimeSelector/TimeSelector.vue
+++ app/components/TimeSelector/TimeSelector.vue
@@ -1,12 +1,27 @@
 <template>
-  <Label class="message" :text="msg" col="0" row="0"/>
+  <FlexboxLayout alignItems="flex-start" alignContent="center">
+    <Label class="message" :text="msg" width="70" height="80"/>
+    <Button class="round-btn" text="+" @tap="increment" />
+    <TextField class="text-input" v-model="value"/>
+    <Button class="round-btn" text="-" @tap="decrement"/>
+  </FlexboxLayout>
 </template>
 
 <script lang="ts">
  import { Component, Vue } from "vue-property-decorator";
  @Component
  export default class TimeSelector extends Vue {
-  msg:string = 'Select Time';
+  msg: string = 'Prep';
+  text: string = '';
+  value: number = 0;
+
+  increment() {
+    return this.value += 1;
+  }
+
+  decrement() {
+    if (this.value > 0) return this.value -= 1;
+  }
  }
 </script>
 
